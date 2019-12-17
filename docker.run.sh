4c126553cac4bfa94e4509b1fa741daba4bdefc8
zetxx <zetxx@belogradchik.biz>
2019-12-17 17:17:47 +0200

    fix: conf

--- docker.run.sh
+++ docker.run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 docker run --init -d --restart=unless-stopped \
--e token_pu_1=$TOKEN \
---name="homeass" -v /home/zetxx/home-assistant-config:/config \
+--env-file=~/home-assistant-config/.env \
+--name="homeass" -v ~/home-assistant-config:/config \
 --net=host homeassistant/aarch64-homeassistant:latest
-
