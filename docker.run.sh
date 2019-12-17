f90c1e8db439f941fb95ace36a35e8a4b01d4fa0
zetxx <zetxx@belogradchik.biz>
2019-12-17 17:25:38 +0200

    fix: net

--- docker.run.sh
+++ docker.run.sh
@@ -2,4 +2,5 @@
 docker run --init -d --restart=unless-stopped \
 --env-file="$HOME/home-assistant-config/.env" \
 --name="homeass" -v "$HOME/home-assistant-config:/config" \
+--net=host \
 --net=host homeassistant/aarch64-homeassistant:latest
