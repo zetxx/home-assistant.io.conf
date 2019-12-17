cd33818ccab5e54fb99b65b903b22cdacc98bed7
zetxx <zetxx@belogradchik.biz>
2019-12-17 17:22:01 +0200

    fix: path

--- docker.run.sh
+++ docker.run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker run --init -d --restart=unless-stopped \
---env-file=~/home-assistant-config/.env \
---name="homeass" -v ~/home-assistant-config:/config \
+--env-file="$HOME/home-assistant-config/.env" \
+--name="homeass" -v "$HOME/home-assistant-config:/config" \
 --net=host homeassistant/aarch64-homeassistant:latest
