5d00b97430308c642b11ebd226093030f9ed0ea0
Elin Angelov <zetxx@belogradchik.biz>
2020-01-14 14:55:37 +0200

    Update docker.run.sh

--- docker.run.sh
+++ docker.run.sh
@@ -3,4 +3,4 @@ docker run --init -d --restart=unless-stopped \
 --env-file="$HOME/home-assistant-config/.env" \
 --name="homeass" -v "$HOME/home-assistant-config:/config" \
 --net=host \
-homeassistant/aarch64-homeassistant:latest
+homeassistant/aarch64-homeassistant:stable
