b859cecf2cbec08db358cdcc560f67a1303d59f5
zetxx <zetxx@belogradchik.biz>
2020-11-05 22:55:54 +0200

    fix: new config

--- docker.run.sh
+++ docker.run.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+# current
+docker run --init -d --restart=unless-stopped \
+--env-file="$HOME/home-assistant-config/.env" \
+--name="homeass" -v "$HOME/home-assistant-config:/config" \
+--net=host \
+homeass-current
+
 docker run --init -d --restart=unless-stopped \
 --env-file="$HOME/home-assistant-config/.env" \
 --name="homeass" -v "$HOME/home-assistant-config:/config" \
